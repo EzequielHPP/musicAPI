cb5d20a978f1f6ac689c8568c2a0f55b34bba724
Ezequiel <info@ezequielhpp.net>
2017-03-24 00:51:24 +0000

    Further improvements on seeders

--- database/factories/TracksFactory.php
+++ database/factories/TracksFactory.php
@@ -13,7 +13,7 @@ $factory->define(App\v1\Models\Tracks::class, function (Faker\Generator $faker) 
         'album_id' => 1,
         'title' => $faker->name,
         'length' => $faker->numberBetween(10,200),
-        'disc_number' => $faker->numberBetween(1,2),
+        'disc_number' => 1,
         'track_order' => $faker->numberBetween(1,10)
     ];
 });
